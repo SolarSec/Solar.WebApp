fccd525b5a4e81d1bc40027f9651a037508742fe
vSync <71209712+0xvsync@users.noreply.github.com>
2022-10-06 18:51:26 +0100

    Update error_handler.py

--- assets/error_handler.py
+++ assets/error_handler.py
@@ -3,13 +3,13 @@
 """
 
 #Handle ARGUMENT error.
-def handle_argerr () -> str:
+def handle_argerr () -> dict:
     return "python {} Webserver_port rate_limit_port debug\n Webserver_port: int, rate_limit_timeout: int, debug: bool"
 
 #Handle No_ID error | Flask
-def flask_noiderr () -> str:
+def flask_noiderr () -> dict:
     return dict({"Error" : "You have to specify a ID. http://..../api/v1/scripts?id=...."})
 
 #Handle Incorrect data_type error | Flask
-def flask_dterror () -> str:
+def flask_dterror () -> dict:
     return dict({"Error" : "The ID parameter you've provided must be a string, not {}"})
